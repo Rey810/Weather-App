52bd40d788b88f3d7816828642bc9ffee731060e
reyvdb <Rey810@users.noreply.github.com>
2020-04-24 15:16:13 +0700

    hotfix colours

--- colors.js
+++ colors.js
@@ -2,7 +2,7 @@ export let colors = {
   Clouds: "hsla(172, 76%, 54%, 0.349)",
   Snow: "hsla(288, 37%, 35%, 0.612)",
   Rain: "hsla(230, 87%, 59%, 0.55)",
-  Clear: "hsla(50, 100%, 55%, 0.769)",
+  Clear: "hsla(0, 100%, 70.6%, 0.5)",
   Windy: "hsla(157, 42%, 46%, 0.6)",
   Thunderstorm: "hsla(269.3, 69.8%, 35.1%, 0.55)",
   Drizzle: "hsla(198.3, 49.4%, 52%, 0.5);",
